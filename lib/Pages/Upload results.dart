a5fd53ce9bf9ff6df5b674e4f72c203b7426d84f
JesusIsthe1 <soniccoolkidfan1o1@gmail.com>
2025-07-24 09:19:38 +1200

    Dropdown menu made 24 july

--- lib/Pages/Upload results.dart
+++ lib/Pages/Upload results.dart
@@ -1,29 +1,49 @@
 import 'package:flutter/material.dart';
 
-class UploadPage extends StatelessWidget {
-  const UploadPage ({super.key})
-  String dropdownValue = '3';
+class UploadPage extends StatefulWidget {
+  const UploadPage({super.key});
+
+  @override
+  State<UploadPage> createState() => _UploadPageState();
+}
+
+class _UploadPageState extends State<UploadPage> {
+  String dropdownValue = 'One';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sample Code')),
-      body: Center(
-          child: DropdownButton<String>(
-            value: dropdownValue,
-            icon: const Icon(Icons.arrow_downward),
-            style: const TextStyle(color: Colors.white),
-            underline: Container(
-              height: 2,
-              color: Colors.white,
-            ),
-            onChanged: (String? newValue) {
-              setState(() {
-                dropdownValue = newValue!;
-              })
-            },
+        appBar: AppBar(title: const Text('Sample Code')),
+        body: Center(
+            child: DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              style: const TextStyle(color: Colors.black54),
+              underline: Container(
+                height: 2,
+                color: Colors.blue,
+              ),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+              items: const[
+                DropdownMenuItem<String>(
+                  value: 'One',
+                  child: Text('One'),
+                ),
+                DropdownMenuItem<String>(
+                    value: 'Two',
+                    child: Text('Two')
+                ),
+                DropdownMenuItem<String>(
+                  value: 'Three',
+                  child: Text('Three'),
+                )
+              ],
+            )
         )
-      )
     );
   }
 }
