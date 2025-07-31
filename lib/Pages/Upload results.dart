5b1ae89f20a06d354092f218081660f07b124c6c
JesusIsthe1 <soniccoolkidfan1o1@gmail.com>
2025-08-01 09:29:01 +1200

    Upload page finished now working on json file

--- lib/Pages/Upload results.dart
+++ lib/Pages/Upload results.dart
@@ -8,42 +8,62 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
-  String dropdownValue = 'One';
+  String dropdownValue = 'Zero';
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Sample Code')),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: const Text('app')),
         body: Center(
-            child: DropdownButton<String>(
-              value: dropdownValue,
-              icon: const Icon(Icons.arrow_downward),
-              style: const TextStyle(color: Colors.black54),
-              underline: Container(
-                height: 2,
-                color: Colors.blue,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                        label: Text('Subject (spell in full)')
+                    ),)
               ),
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownValue = newValue!;
-                });
-              },
-              items: const[
-                DropdownMenuItem<String>(
-                  value: 'One',
-                  child: Text('One'),
+              Container(
+                  child: TextField(
+                    maxLength: 10,
+                    decoration: const InputDecoration(
+                        label: Text('Standard Number')
+                    ),)
+              ),
+              Container(
+                child: DropdownButton<String>(
+                  value: dropdownValue,
+                  icon: const Icon(Icons.arrow_downward),
+                  style: const TextStyle(color: Colors.black54),
+                  underline: Container(height: 2, color: Colors.blue),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  items: const [
+                    DropdownMenuItem<String>(
+                      value: 'Zero',
+                      child: Text('How many credits?'),
+                    ),
+                    DropdownMenuItem<String>(value: 'One', child: Text('One')),
+                    DropdownMenuItem<String>(value: 'Two', child: Text('Two')),
+                    DropdownMenuItem<String>(
+                      value: 'Three',
+                      child: Text('Three'),
+                    ),
+                  ],
                 ),
-                DropdownMenuItem<String>(
-                    value: 'Two',
-                    child: Text('Two')
-                ),
-                DropdownMenuItem<String>(
-                  value: 'Three',
-                  child: Text('Three'),
-                )
-              ],
-            )
-        )
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/Thingy.dart
+++ lib/Thingy.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_project/Pages/Qualifications.dart';
 import 'package:my_project/Pages/Home page (dashboard).dart';
 import 'package:my_project/Pages/Upload results.dart';
-
+//Imported all of the pages so it can call upon them
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -27,14 +27,14 @@ class _MainPageState extends State<MainPage> {
       body: pages[currentpage],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentpage,
-        onTap: (value) {setState(() {
+        onTap: (value) {setState(() {      //When it is clicked it will change to which ever value you clicked
           currentpage = value;
         });},
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home",),
           BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: "Qualifications",),
           BottomNavigationBarItem(icon: Icon(Icons.file_upload_outlined), label: "Upload Results",),
-        ], // This Items list is the list of
+        ], // This Items list is the list of the different pages and how they are displayed in the navigationbar
       ),
     );
   }
