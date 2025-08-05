aaddcdcb3a2cb466c7ce279f6694a588f6e9eaa3
JesusIsthe1 <soniccoolkidfan1o1@gmail.com>
2025-08-05 22:08:21 +1200

    5th august Json file save made

--- lib/Pages/Upload results.dart
+++ lib/Pages/Upload results.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
 class UploadPage extends StatefulWidget {
   const UploadPage({super.key});
@@ -9,7 +12,30 @@ class UploadPage extends StatefulWidget {
 
 class _UploadPageState extends State<UploadPage> {
   String dropdownValue = 'Zero';
+  String subject = '';
+  String standard = '';
+  late File jsonFile;
+  List<dynamic> upload = [];
 
+  @override
+  void initSate() {
+    super.initState();
+    _initFile();
+  }
+
+  Future<void> _initFile() async {
+    final dir = await getApplicationDocumentsDirectory();
+    jsonFile = File('${dir.path}/upload.json');
+
+    if (!await jsonFile.exists()) {
+      await jsonFile.writeAsString(json.encode([]));
+    }
+  }
+
+  Future<void> _addUpload(String subject, String standard) async {
+    upload.add({'subject': subject, 'standard': standard});
+    await jsonFile.writeAsString(json.encode(upload)); //sorts the file 
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,17 +49,24 @@ class _UploadPageState extends State<UploadPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Container(
-                  child: TextField(
-                    decoration: const InputDecoration(
-                        label: Text('Subject (spell in full)')
-                    ),)
+                child: TextField(
+                  onChanged:
+                      (value) =>
+                          subject =
+                              value, //When changed it will add it to the variable
+                  decoration: const InputDecoration(
+                    label: Text('Subject (spell in full)'),
+                  ),
+                ),
               ),
               Container(
-                  child: TextField(
-                    maxLength: 10,
-                    decoration: const InputDecoration(
-                        label: Text('Standard Number')
-                    ),)
+                child: TextField(
+                  onChanged: (value) => standard = value,
+                  maxLength: 10,
+                  decoration: const InputDecoration(
+                    label: Text('Standard Number'),
+                  ),
+                ),
               ),
               Container(
                 child: DropdownButton<String>(
@@ -58,6 +91,16 @@ class _UploadPageState extends State<UploadPage> {
                       child: Text('Three'),
                     ),
                   ],
+                ),
+              ),
+              Container(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (subject.isNotEmpty && standard.isNotEmpty) {
+                      _addUpload(subject, standard);
+                    }
+                  },
+                  child: const Text('save'),
                 ),
               ),
             ],
